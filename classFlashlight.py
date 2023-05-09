9baec4f806f79673a8f7ff5813a989c4c9d88498
1GodLess5 <gacekalbert@seznam.cz>
2023-05-09 21:15:50 +0200

    simple class in Python

---
+++ classFlashlight.py
@@ -0,0 +1,10 @@
+class Flashlight:
+    def __init__(self, color, isOn):
+        self.color = color
+        self.isOn = isOn
+
+    def turnOn(self):
+        self.isOn = True
+
+    def turnOff(self):
+        self.isOn = False
